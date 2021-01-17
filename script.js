737e97e7fbe44695c78886c1e203ba1ea52026a3
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-17 17:25:52 +0200

    Re-initiating scripts for desktop view

--- script.js
+++ script.js
@@ -1,3 +1,119 @@
+// DESKTOP SCRIPTS
+const slidesContainer = document.getElementById("desktop-slides-container");
+const cardGroup = document.querySelectorAll(".card-group");
+const nextBtn = document.querySelector(".next-btn.arrow");
+const prevBtn = document.querySelector(".prev-btn.arrow");
+const navigationDots = document.getElementById("desktop-navigation-dots");
+
+let numberOfCardGroups = cardGroup.length;
+let slideWidth = cardGroup[0].clientWidth;
+let currentSlide = 0;
+
+// Set up the slider
+
+function init() {
+  /*   
+    cardGroup[0] = 0
+    cardGroup[1] = 100%
+    cardGroup[2] = 200%
+    */
+
+  cardGroup.forEach((card, i) => {
+    card.style.left = i * 100 + "%";
+  });
+
+  cardGroup[0].classList.add("active");
+
+  createNavigationDots();
+}
+
+init();
+
+// Create navigation dots
+
+function createNavigationDots() {
+  for (let i = 0; i < numberOfCardGroups; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("single-dot");
+    navigationDots.appendChild(dot);
+
+    dot.addEventListener("click", () => {
+      goToSlide(i);
+    });
+  }
+
+  navigationDots.children[0].classList.add("active");
+}
+
+// Next Button
+
+nextBtn.addEventListener("click", () => {
+  if (currentSlide >= numberOfCardGroups - 1) {
+    goToSlide(0);
+    return;
+  }
+
+  currentSlide++;
+  goToSlide(currentSlide);
+});
+
+// Previous Button
+
+prevBtn.addEventListener("click", () => {
+  if (currentSlide <= 0) {
+    goToSlide(numberOfCardGroups - 1);
+    return;
+  }
+
+  currentSlide--;
+  goToSlide(currentSlide);
+});
+
+// Go To Slide
+
+function goToSlide(slideNumber) {
+  slidesContainer.style.transform =
+    "translateX(-" + slideWidth * slideNumber + "px)";
+
+  currentSlide = slideNumber;
+
+  setActiveClass();
+}
+
+// Set Active Class
+
+function setActiveClass() {
+  // Set active class for Slide Image
+
+  let currentActive = document.querySelector(".card-group.active");
+  currentActive.classList.remove("active");
+  cardGroup[currentSlide].classList.add("active");
+
+  //   set active class for navigation dots
+
+  let currentDot = document.querySelector(".single-dot.active");
+  currentDot.classList.remove("active");
+  navigationDots.children[currentSlide].classList.add("active");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // SCRIPTS FOR MOBILE VIEW
 const mobileSlidesContainer = document.querySelector(".mobile-slides-container");
